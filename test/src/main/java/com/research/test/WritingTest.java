b17938ede948217dd77aaff687f286b914e08b6d
spadem45420 <spadem45420@gmail.com>
2016-09-21 10:41:29 +0800

    Jun hong's problem about the java writing format

---
+++ test/src/main/java/com/research/test/WritingTest.java
@@ -0,0 +1,13 @@
+package com.research.test;
+
+public class WritingTest {
+
+	public static void main(String[] args) {
+//		String test = new String(), test1, test2, test3;
+//		String test = new String();
+//		String test1;
+//		String test2;
+//		String test3;
+	}
+
+}
