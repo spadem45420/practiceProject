46c51de52cea2bc2f54df20a7a0d4eedb319732d
spadem45420 <spadem45420@gmail.com>
2016-09-21 10:40:10 +0800

    Kyle's problem about json parse but not use third party api. This is not working ...

---
+++ test/src/main/java/com/json/test/JSONParsingTest.java
@@ -0,0 +1,30 @@
+package com.json.test;
+
+public class JSONParsingTest {
+
+//	private ScriptEngine engine;
+//
+//    @Before
+//    public void initEngine() {
+//        ScriptEngineManager sem = new ScriptEngineManager();
+//        this.engine = sem.getEngineByName("javascript");
+//    }
+//
+//    @Test
+//    public void parseJson() throws IOException, ScriptException {
+//        String json = new String(Files.readAllBytes(/*path*/);
+//        String script = "Java.asJSONCompatible(" + json + ")";
+//        Object result = this.engine.eval(script);
+//        assertThat(result, instanceOf(Map.class));
+//        Map contents = (Map) result;
+//        contents.forEach((t, u) -> {
+//        //key-value pairs
+//        });
+//    }
+//	
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//
+//	}
+
+}
